ee314b4dc38cfa006a6ca9efb296bd2daa9911c1
Dennis A.S <bencyshaju2005@gmail.com>
2025-01-31 10:18:08 +0530

    updatedata

--- index.js
+++ index.js
@@ -12,16 +12,29 @@ app.post("/",(req,res)=>{
         res.send(error);
        }
     });
-
+    app.get('/',async(req,res)=>{
+        var data = await productModel.find(); 
+        res.send(data);try{ 
+        } catch (error) {
+            res.send(error);
+        } 
+    });
     app.delete('/remove/:id',async(req,res)=>{
         try {
         await productModal.findByIdAndDelete(req.params.id);
         res.send("delete successfully!!");
         } catch (error) {
             res.send(error);
-        }
-        
+        } 
     })
+    app.put('/edit/:id',async(req,res)=>{
+        try {
+            await productModel.findByIdAndUpdate(req.params.id,req.body);
+            res.send("updated sucessfully");
+       } catch (error){
+        res.send(error);
+       }
+    });
 
 app.listen(PORT,(req,res) =>{
     console.log(`server is listening in port ${PORT}`);
